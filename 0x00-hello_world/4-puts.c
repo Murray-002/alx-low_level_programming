cf80430c991969236a56f4dc479e80193b65e4d4
Murray-002 <odhiambomurray@gmail.com>
2022-03-10 12:22:25 -0800

    fix documentaion in main function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,14 @@
 /*
  * main-entry
  *
- * return -alwys 0
+ * return -alwiays 0
  */
 int main(void)
 {
 /*
- * main- prints as directed,and adds new line
+ * main -Prints "Programming is like building a multilingual
+ *
+ *                 puzzle, followed by a new line.
  */
 	puts("\"Programming is like building a multilingual puzzle");
 return (0);
