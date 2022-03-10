36cd36652dbac5dde630d0c2893a5da4d6f0fd92
Murray-002 <odhiambomurray@gmail.com>
2022-03-10 12:13:51 -0800

    add documentation to main

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,9 +4,12 @@
  * main-entry
  *
  * return -alwys 0
- * */
+ */
 int main(void)
 {
+/*
+ * main- prints as directed,and adds new line
+ */
 	puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
